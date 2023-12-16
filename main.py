4b84d9e489941c41e65e862d379374e1e5f2f48b
Matteo Andreuzza <76852004+matteo-andreuzza@users.noreply.github.com>
2023-12-16 23:44:28 +0100

    somma funzionante con casi particolari di esponenti, molt ancora in alto mare

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ a = ''
 b = ''
 
 def molt(x,y):
+    ret = ''
     q = ''
     q2 = ''
     w = ''
@@ -18,7 +19,27 @@ def molt(x,y):
                 w += i
         except ValueError:
             w2 += i
-    return str((int(q)*int(w))) + q2 + w2
+    r = str((int(q)*int(w))) + q2 + w2
+    print('r' + r)
+    es = 0
+    ret = list(r)
+    for i in range(len(ret)):
+        try:
+            if ret[i] == ret[i+1]:
+                for k in range(len(ret)-i):
+                    try:
+                        if ret[k] == ret[k+1]:
+                            es = es + 1
+                            ret.pop(k+1)
+                    except IndexError:
+                        pass
+                ret[i] = ret[i] + '^' + str(es+1)
+        except IndexError:
+            pass
+    r = ''
+    for i in ret:
+        r += i
+    return r
 op = ['-']
 num = ['1','2','3','4','5','6','7','8','9']
 lett = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -59,6 +80,8 @@ def sum(x,y):
     q2 = ''
     w = ''
     w2 = ''
+    esp1 = 0
+    esp2 = 0
     c = 0
     if x[0] != "-" and x[0] not in num:
         t = list(x)
@@ -72,30 +95,43 @@ def sum(x,y):
         y = ''
         for z in t:
             y += str(z)
+    for i in range(len(x)):
+        if x[i] == '^':
+            esp1 = i
     for i in x:
-        try:
-            if i == '-':
-                continue
-            if type(int(i)) == int and x[c] == '-':
-                q += '-'+i
-            else:
-                q += i
-        except ValueError:
+        if c < esp1:
+            try:
+                if i == '-':
+                    continue
+                if type(int(i)) == int and x[c] == '-':
+                    q += '-'+i
+                else:
+                    q += i
+            except ValueError:
+                q2 += i
+        else:
             q2 += i
         c = c+1
     c = 0
     i = 0
+    for i in range(len(y)):
+        if y[i] == '^':
+            esp2 = i
     for i in y:
-        try:
-            if i == '-':
-                continue
-            if type(int(i)) == int and y[c] == '-':
-                w += '-'+i
-            else:
-                w +=i
-            
-        except ValueError:
+        if c < esp2:
+            try:
+                if i == '-':
+                    continue
+                if type(int(i)) == int and y[c] == '-':
+                    w += '-'+i
+                else:
+                    w +=i
+                
+            except ValueError:
+                w2 += i
+        else:
             w2 += i
+            
         c = c+1
     if q2 == w2 and int(q)+int(w) != 0:
         return str((int(q)+int(w))) + q2
@@ -103,4 +139,5 @@ def sum(x,y):
         return ''
     else:
         return 'impossibile sommare termini con lettere diverse'
-print("somma " + str(algsum(['7ab', '16ab', '-2ab', '4ab'])))
+
+print(molt('4ab', '2ab'))
